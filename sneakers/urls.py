ac6647ee3a41d90769a0272936b16f8c9ab15c9e
Monica Banda <monicabanda@Monicas-Air.lan>
2022-10-17 16:12:38 -0600

    Created New View Function for Getting Sneakers By Id + Added New URLs Path for sneaker_detail View Function

--- sneakers/urls.py
+++ sneakers/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.sneakers_list),
+    path('<int:pk>/', views.sneaker_detail),
 ]
--- sneakers/views.py
+++ sneakers/views.py
@@ -3,18 +3,26 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import Sneakerserializer
 from .models import Sneaker
+from sneakers import serializers
 
 @api_view(['GET', 'POST'])
 def sneakers_list(request):
-
     if request.method == 'GET':
         sneakers = Sneaker.objects.all()
         serializer = Sneakerserializer(sneakers, many=True)
-        return Response(serializer.data)
-    
-    
+        return Response(serializer.data)   
     elif request.method == 'POST':
         serializer = Sneakerserializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def sneaker_detail(request, pk):
+    try:
+        sneaker = Sneaker.objects.get(pk=pk)
+        serializer = Sneakerserializer(sneaker);
+        return Response(serializer.data)
+
+    except Sneaker.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND);
